6d81c372d25289e8bc69c36fde9d82d493396bc6
dongw00 <albadw0428@gmail.com>
2021-01-02 12:29:53 +0900

    TIL

--- algorithm/greedy/Beak1449.java
+++ algorithm/greedy/Beak1449.java
@@ -1,33 +1,35 @@
 package greedy;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Beak1449 {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
 
-		int arr[] = new int[N];
+        int[] arr = new int[N];
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
 
-		Arrays.sort(arr);
+        Arrays.sort(arr);
 
-		int count = 1;
-		int temp = arr[0];
-		for (int i = 0; i < N; i++) {
-			if (temp + L - 1 < arr[i]) {
-				temp = arr[i];
-				count++;
-			}
-		}
+        int count = 1;
+        int temp = arr[0];
+        for (int i = 1; i < N; i++) {
+            if (temp + L - 1 < arr[i]) {
+                temp = arr[i];
+                count++;
+            }
+        }
 
-		System.out.println(count);
-	}
+        System.out.println(count);
+    }
 }
--- algorithm/greedy/Beak4796.java
+++ algorithm/greedy/Beak4796.java
@@ -1,27 +1,28 @@
 package greedy;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Beak4796 {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		int idx = 1;
+        int idx = 1;
 
-		while (true) {
-			st = new StringTokenizer(br.readLine());
-			int L = Integer.parseInt(st.nextToken());
-			int P = Integer.parseInt(st.nextToken());
-			int V = Integer.parseInt(st.nextToken());
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
 
-			if (L == 0 && P == 0 && V == 0)
-				break;
+            if (L == 0 && P == 0 && V == 0)
+                break;
 
-			int temp = (V % P > L) ? L : V % P;
-			int result = (V / P) * L + temp;
-			System.out.println("Case " + idx++ + ": " + result);
-		}
-	}
+            int temp = Math.min(V % P, L);
+            int result = (V / P) * L + temp;
+            System.out.println("Case " + idx++ + ": " + result);
+        }
+    }
 }

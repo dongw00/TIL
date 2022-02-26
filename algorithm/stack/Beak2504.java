e19d4ca95a8eed6efff69792a7c5fa6dc0a5b7e8
dongw00 <albadw0428@gmail.com>
2022-02-26 19:36:36 +0900

    TIL

--- algorithm/stack/Beak2504.java
+++ algorithm/stack/Beak2504.java
@@ -1,58 +1,75 @@
 package stack;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Beak2504 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split("");
-
-        if (str.length % 2 == 1) {
-            System.out.println(0);
-            System.exit(0);
-        }
+        String str = br.readLine();
 
         Stack<String> stk = new Stack<>();
-        int ans = 0;
-        int tmp = 1;
-        for (int i = 0; i < str.length; i++) {
-            switch (str[i]) {
-                case "(":
-                    tmp *= 2;
-                    stk.push("(");
-                    break;
-                case "[":
-                    tmp *= 3;
-                    stk.push("[");
-                    break;
-                case ")":
-                    if (stk.isEmpty() || !stk.peek().equals("(")) {
-                        System.out.println(0);
-                        System.exit(0);
-                    }
-                    if (str[i - 1].equals("(")) {
-                        ans += tmp;
-                    }
-                    stk.pop();
-                    tmp /= 2;
-                    break;
-                case "]":
-                    if (stk.isEmpty() || !stk.peek().equals("[")) {
-                        System.out.println(0);
-                        System.exit(0);
-                    }
-                    if (str[i - 1].equals("[")) {
-                        ans += tmp;
-                    }
-                    stk.pop();
-                    tmp /= 3;
-                    break;
+
+        try {
+            for (char c : str.toCharArray()) {
+                switch (c) {
+                    case ')':
+                        if (stk.peek().equals("(")) {
+                            stk.pop();
+                            stk.push("2");
+                            break;
+                        } else if (!stk.peek().equals("[")) {
+                            int sum = 0;
+                            while (!stk.peek().equals("(") && !stk.peek().equals("[")) {
+                                sum += Integer.parseInt(stk.pop());
+                            }
+                            if (!stk.peek().equals("(")) {
+                                throw new Exception();
+                            }
+                            stk.pop();
+                            stk.push(Integer.toString(sum * 2));
+                        } else {
+                            throw new Exception();
+                        }
+                        break;
+                    case ']':
+                        if (stk.peek().equals("[")) {
+                            stk.pop();
+                            stk.push("3");
+                            break;
+                        } else if (!stk.peek().equals("(")) {
+                            int sum = 0;
+                            while (!stk.peek().equals("(") && !stk.peek().equals("[")) {
+                                sum += Integer.parseInt(stk.pop());
+                            }
+                            if (!stk.peek().equals("[")) {
+                                throw new Exception();
+                            }
+                            stk.pop();
+                            stk.push(Integer.toString(sum * 3));
+                        } else {
+                            throw new Exception();
+                        }
+                        break;
+                    default:
+                        stk.push(Character.toString(c));
+                        break;
+                }
             }
+
+            int ans = 0;
+            while (!stk.isEmpty()) {
+                if (stk.peek().equals("(") || stk.peek().equals("[")) {
+                    throw new Exception();
+                }
+                ans += Integer.parseInt(stk.pop());
+            }
+            System.out.println(ans);
+        } catch (Exception e) {
+            System.out.println(0);
         }
-        System.out.println(ans);
+
     }
 }

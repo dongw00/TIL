d1654ec79874b3ebe27ae3e6baa6c3878cf11d08
dongw00 <albadw0428@gmail.com>
2022-07-05 14:38:05 +0900

    TIL

--- algorithm/segmentTree/Beak11505.java
+++ algorithm/segmentTree/Beak11505.java
@@ -7,9 +7,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Beak2042 {
+public class Beak11505 {
 
-    static long[] arr, tree;
+    static final int MOD = 1000000007;
+    static int[] arr;
+    static long[] tree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,11 +21,11 @@ public class Beak2042 {
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        arr = new long[n];
+        arr = new int[n];
         tree = new long[n * 4];
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Long.parseLong(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         init(0, n - 1, 1);
@@ -31,20 +33,22 @@ public class Beak2042 {
         m += k;
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         while (m-- > 0) {
             st = new StringTokenizer(br.readLine());
             int mod = Integer.parseInt(st.nextToken());
 
-            int b = Integer.parseInt(st.nextToken()) - 1;
-
             if (mod == 1) {
-                long c = Long.parseLong(st.nextToken());
+                int b = Integer.parseInt(st.nextToken()) - 1;
+                int num = Integer.parseInt(st.nextToken());
 
-                arr[b] = c;
-                update(0, n - 1, 1, b, c);
+                arr[b] = num;
+                update(0, n - 1, 1, b, num);
             } else {
+                int b = Integer.parseInt(st.nextToken()) - 1;
                 int c = Integer.parseInt(st.nextToken()) - 1;
-                bw.write(sum(0, n - 1, b, c, 1) + "\n");
+
+                bw.write(multiply(0, n - 1, b, c, 1) + "\n");
             }
         }
         bw.flush();
@@ -55,25 +59,12 @@ public class Beak2042 {
         if (start == end) {
             return tree[node] = arr[start];
         }
-        int mid = (start + end) / 2;
-        return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
-    }
-
-    public static long sum(int start, int end, int left, int right, int node) {
-        if (left > end || right < start) {
-            return 0;
-        }
-
-        if (left <= start && end <= right) {
-            return tree[node];
-        }
 
         int mid = (start + end) / 2;
-        return sum(start, mid, left, right, node * 2) + sum(mid + 1, end, left, right,
-            node * 2 + 1);
+        return tree[node] = (init(start, mid, node * 2) * init(mid + 1, end, node * 2 + 1)) % MOD;
     }
 
-    public static long update(int start, int end, int node, int idx, long num) {
+    public static long update(int start, int end, int node, int idx, int num) {
         if (idx < start || end < idx) {
             return tree[node];
         }
@@ -83,8 +74,23 @@ public class Beak2042 {
         }
 
         int mid = (start + end) / 2;
-        return tree[node] = update(start, mid, node * 2, idx, num) +
-            update(mid + 1, end, node * 2 + 1, idx, num);
+        return tree[node] =
+            (update(start, mid, node * 2, idx, num) * update(mid + 1, end, node * 2 + 1, idx,
+                num))
+                % MOD;
     }
 
+    public static long multiply(int start, int end, int left, int right, int node) {
+        if (left > end || right < start) {
+            return 1;
+        }
+
+        if (left <= start && end <= right) {
+            return tree[node];
+        }
+
+        int mid = (start + end) / 2;
+        return (multiply(start, mid, left, right, node * 2) * multiply(mid + 1, end, left, right,
+            node * 2 + 1)) % MOD;
+    }
 }
